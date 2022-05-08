91c83af3a55620947ccb4e1772ea7279f4ec579e
Stefan Igor Fekete <85646718+Stefan-Igor-Fekete@users.noreply.github.com>
2022-05-08 18:08:49 +0300

    Denis check it

---
+++ Task1.1/Task1.1/src/main/java/framework/elements/TextField.java
@@ -0,0 +1,19 @@
+package framework.elements;
+
+import framework.base.BaseElement;
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TextField extends BaseElement {
+    private static final Logger logger = LoggerFactory.getLogger(TextField.class);
+
+    public TextField(By locator, String name) {
+        super(locator, name);
+    }
+
+    public void sendText(String text) {
+        logger.info("Using sendTextMethod");
+        findElement().sendKeys(text);
+    }
+}
